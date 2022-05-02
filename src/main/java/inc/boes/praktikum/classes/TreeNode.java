2d36d25356a0d67ecdbd0586d00f30dd503cc1e9
Timon Stronczek <s6tnstro@uni-bonn.de>
2022-05-02 23:56:23 +0200

    Added all variables and functions needed in TreeNode.

--- src/main/java/inc/boes/praktikum/classes/TreeNode.java
+++ src/main/java/inc/boes/praktikum/classes/TreeNode.java
@@ -1,5 +1,35 @@
 package inc.boes.praktikum.classes;
 
 public class TreeNode<T extends Number> {
+    private T root;
+    private TreeNode<T> leftChild;
+    private TreeNode<T> rightChild;
 
+    public TreeNode(T pRoot){
+        this.root = pRoot;
+    }
+
+    public T getRoot(){
+        return root;
+    }
+
+    public void setRoot(T pRoot){
+        this.root = pRoot;
+    }
+
+    public TreeNode<T> getLeftChild(){
+        return leftChild;
+    }
+
+    public void setLeftChild(TreeNode<T> pLeft){
+        this.leftChild = pLeft;
+    }
+
+    public TreeNode<T> getRightChild(){
+        return rightChild;
+    }
+
+    public void setRightChild(TreeNode<T> pRight){
+        this.rightChild = pRight;
+    }
 }
