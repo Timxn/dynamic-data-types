47aa775a2ca91d75e5936bc16b75b4d670d15c5c
Timon Stronczek <s6tnstro@uni-bonn.de>
2022-05-03 00:15:00 +0200

    Changed name and implemented basic structure as well as all functions from the interface. Giving an error at the override of the functions with a parameter.

---
+++ src/main/java/inc/boes/praktikum/classes/BinarySearchTree.java
@@ -0,0 +1,34 @@
+package inc.boes.praktikum.classes;
+
+import inc.boes.praktikum.interfaces.AbstractBinarySearchTree;
+
+public class BinarySearchTree<T extends Number> implements AbstractBinarySearchTree {
+    public static void BinarySearchTree(String[] args) {
+        System.out.println("Hello World");
+    }
+
+    @Override
+    public void insert(T data) {
+
+    }
+
+    @Override
+    public void delete(T data) {
+
+    }
+
+    @Override
+    public boolean search(T data) {
+        return false;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public String toString(String Traversal) {
+        return null;
+    }
+}
