dc2ff2a7229c8642908b3efcf48a9640726515d2
Timon Stronczek <s6tnstro@uni-bonn.de>
2022-05-03 00:05:27 +0200

    Changed formatting

--- src/main/java/inc/boes/praktikum/interfaces/AbstractBinarySearchTree.java
+++ src/main/java/inc/boes/praktikum/interfaces/AbstractBinarySearchTree.java
@@ -4,8 +4,6 @@ public interface AbstractBinarySearchTree<T extends Comparable> {
     void insert(T data);
     void delete(T data);
     boolean search(T data);
-
     boolean isEmpty();
-
     String toString(String Traversal);
 }
