0debdb0d0d0c81bacc70225809b8c4e03743897d
mayerb0 <s6bomaye@uni-bonn.de>
2022-05-03 11:08:52 +0200

    added abstract to name and corrected methods

---
+++ src/main/java/inc/boes/praktikum/interfaces/AbstractQueue.java
@@ -0,0 +1,8 @@
+package inc.boes.praktikum.interfaces;
+
+public interface AbstractQueue <T>{
+    public abstract void enqueue (T Node);  //adds to the queue
+    public abstract T dequeue ();           //removes oldest entry
+    public abstract int size();             //gives legth of the queue back
+
+}
--- src/main/java/inc/boes/praktikum/interfaces/Queue.java
+++
@@ -1,8 +0,0 @@
-package inc.boes.praktikum.interfaces;
-
-public interface Queue <T>{
-    void addLast (T Node);
-    void removeFirst (T Node);
-    void remove (T Node, int pointer);          //removes first occurrence of Node
-
-}
