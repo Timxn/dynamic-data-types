888743ec9851a130daf59bcbd74f8c91b2de0d70
justuspfaue <justus@pfaue.eu>
2022-05-04 08:58:04 +0200

    change interface to accommodate for iterators

--- src/main/java/inc/boes/praktikum/interfaces/AbstractStack.java
+++ src/main/java/inc/boes/praktikum/interfaces/AbstractStack.java
@@ -4,4 +4,8 @@ public interface AbstractStack<T> {
     public void push(T object);
     public T pop();
     public T top();
+
+    public boolean hasNext();
+    public T next();
+    public void remove();   // UnsopportedOperationException and IllegalStateException could be thrown
 }
