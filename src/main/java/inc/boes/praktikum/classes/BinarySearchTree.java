7665defdd900b73bb4889ddef40451ee6a88e9e2
Timon Stronczek <s6tnstro@uni-bonn.de>
2022-05-08 12:51:32 +0200

    Idk

--- src/main/java/inc/boes/praktikum/classes/BinarySearchTree.java
+++ src/main/java/inc/boes/praktikum/classes/BinarySearchTree.java
@@ -9,27 +9,58 @@ public class BinarySearchTree<T extends Number> implements AbstractBinarySearchT
     }
 
     @Override
-    public void insert(T data) {
+    public void insert(T value) {
+        root = insertion(root);
+    }
+
+    private TreeNode<T> insertion(TreeNode<T> pNode) {
+        if (isEmpty(pNode)) {
+            return pNode;
+        } else if(pNode.getValue().doubleValue() >= pNode.getValue().doubleValue()) {
+            pNode.setLeftChild(insertion(pNode.getLeftChild()));
+            return pNode.getLeftChild();
+        } else {
+            pNode.setRightChild(insertion(pNode.getRightChild()));
+            return pNode.getRightChild();
+        }
+    }
+
+    @Override
+    public void delete(T value) {
 
     }
 
     @Override
-    public void delete(T data) {
-
-    }
-
-    @Override
-    public boolean search(T data) {
+    public boolean search(T value) {
         return false;
     }
 
     @Override
-    public boolean isEmpty() {
-        return false;
+    public boolean isEmpty(TreeNode<T> pNode) {
+        return pNode == null;
     }
 
     @Override
-    public String toString(String Traversal) {
+    public String toString(String traversal) {
         return null;
     }
+
+    public void toStrings(TreeNode<T> pNode){
+        if(!isEmpty(pNode.getLeftChild())) {
+            toStrings(pNode.getLeftChild());
+        } else if(!isEmpty(pNode)) {
+            System.out.println(pNode.getValue().doubleValue());
+        } else if(!isEmpty(pNode.getRightChild())){
+            toStrings(pNode.getRightChild());
+        }
+    }
+
+    /*public static void main(String[] args) {
+        BinarySearchTree<Integer> bst = new BinarySearchTree<>();
+        bst.insert(2);
+        bst.insert(3);
+        bst.insert(4);
+        bst.insert(1);
+        bst.toStrings(root);
+    }*/
 }
--- src/main/java/inc/boes/praktikum/interfaces/AbstractBinarySearchTree.java
+++ src/main/java/inc/boes/praktikum/interfaces/AbstractBinarySearchTree.java
@@ -1,9 +1,11 @@
 package inc.boes.praktikum.interfaces;
 
+import inc.boes.praktikum.classes.TreeNode;
+
 public interface AbstractBinarySearchTree<T extends Number> {
-    void insert(T data);
-    void delete(T data);
-    boolean search(T data);
-    boolean isEmpty();
-    String toString(String Traversal);
+    void insert(T value);
+    void delete(T value);
+    boolean search(T value);
+    boolean isEmpty(TreeNode<T> pNode);
+    String toString(String traversal);
 }
