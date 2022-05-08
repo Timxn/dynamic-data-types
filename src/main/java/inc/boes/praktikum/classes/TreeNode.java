04e0bb128d648c9393e3bdb60b5fa17057a9810c
Timon Stronczek <s6tnstro@uni-bonn.de>
2022-05-08 12:46:21 +0200

    Changed method and variable names

--- src/main/java/inc/boes/praktikum/classes/TreeNode.java
+++ src/main/java/inc/boes/praktikum/classes/TreeNode.java
@@ -1,22 +1,22 @@
 package inc.boes.praktikum.classes;
 
 public class TreeNode<T extends Number> {
-    private T root;
+    private T value;
     private TreeNode<T> leftChild;
     private TreeNode<T> rightChild;
 
-    public TreeNode(T pRoot){
-        this.root = pRoot;
+    public TreeNode(T value){
+        this.value = value;
         this.leftChild = null;
         this.rightChild = null;
     }
 
-    public T getRoot(){
-        return root;
+    public T getValue(){
+        return value;
     }
 
-    public void setRoot(T pRoot){
-        this.root = pRoot;
+    public void setValue(T value){
+        this.value = value;
     }
 
     public TreeNode<T> getLeftChild(){
