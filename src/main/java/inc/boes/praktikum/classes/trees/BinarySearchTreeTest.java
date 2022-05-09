0463c9eebd170294f265e468dc3212fb3573f008
Timon Stronczek <s6tnstro@uni-bonn.de>
2022-05-09 15:11:22 +0200

    Added test class for bst

---
+++ src/main/java/inc/boes/praktikum/classes/trees/BinarySearchTreeTest.java
@@ -0,0 +1,32 @@
+package inc.boes.praktikum.classes.trees;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BinarySearchTreeTest {
+
+    @Test
+    void insert() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void search() {
+    }
+
+    @Test
+    void isEmpty() {
+    }
+
+    @Test
+    void testToString() {
+    }
+
+    @Test
+    void iterator() {
+    }
+}
