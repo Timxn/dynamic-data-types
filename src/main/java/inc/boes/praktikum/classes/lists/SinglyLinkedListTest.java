ea5f403347c81d3c7dfd15c63fd61ecf8832e14e
mayerb0 <s6bomaye@uni-bonn.de>
2022-05-10 01:54:32 +0200

    Renamed file

--- src/main/java/inc/boes/praktikum/classes/lists/SinglyLinkedListTest.java
+++ src/main/java/inc/boes/praktikum/classes/lists/SinglyLinkedListTest.java
@@ -1,19 +1,111 @@
 package inc.boes.praktikum.classes.lists;
 
-public class SinglyLinkedListTest {
-    static SingleLinkedList<Integer> linkedList;
-    public static void main(String[] args) {
-        System.out.println("Hallo");
-        System.out.println("SingleLinkedList");
-        linkedList = new SingleLinkedList<>(42);
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.* ;
 
-        linkedList.add(24);
-        System.out.println(linkedList.toString());
+import java.util.Iterator;
 
-        linkedList.insertafter(44, 0);
-        System.out.println(linkedList.toString());
+class SinglyLinkedListTest {
 
-        System.out.println("Hello");
+    @Test
+    void add() {
+        SingleLinkedList<Integer> intsingleLinkedList = new SingleLinkedList<>();
+        assertTrue(intsingleLinkedList.isEmpty());
+        intsingleLinkedList.add(5);
+        assertFalse(intsingleLinkedList.isEmpty());
+    }
 
+    @Test
+    void insertAfter() {
+        SingleLinkedList<Integer> intsingleLinkedList = new SingleLinkedList<>();
+        intsingleLinkedList.add((int)(Math.random()*Integer.MAX_VALUE));
+        intsingleLinkedList.add((int)(Math.random()*Integer.MAX_VALUE));
+        intsingleLinkedList.add((int)(Math.random()*Integer.MAX_VALUE));
+        int value = (int)(Math.random()*Integer.MAX_VALUE);
+        intsingleLinkedList.insertafter(value ,2);
+        assertEquals(value, intsingleLinkedList.getNode(3));
+    }
+
+    @Test
+    void removeLast() {
+        SingleLinkedList<Integer> intsingleLinkedList = new SingleLinkedList<>();
+        intsingleLinkedList.add((int)(Math.random()*Integer.MAX_VALUE));
+        intsingleLinkedList.add((int)(Math.random()*Integer.MAX_VALUE));
+        intsingleLinkedList.add((int)(Math.random()*Integer.MAX_VALUE));
+        intsingleLinkedList.removeLast();
+        assertEquals(intsingleLinkedList.getSize(),2);
+    }
+
+    @Test
+    void removeatPointer() {
+        SingleLinkedList<Integer> intsingleLinkedList = new SingleLinkedList<>();
+        intsingleLinkedList.add((int)(Math.random()*Integer.MAX_VALUE));
+        intsingleLinkedList.add((int)(Math.random()*Integer.MAX_VALUE));
+        int temp = (int)(Math.random()*Integer.MAX_VALUE);
+        intsingleLinkedList.add(temp);
+        intsingleLinkedList.removeatPointer(2);
+        assertEquals(temp, intsingleLinkedList.getNode(2));
+
+    }
+
+
+    @Test
+    void removeNode() {
+        SingleLinkedList<Integer> intsingleLinkedList = new SingleLinkedList<>();
+        intsingleLinkedList.add((int)(Math.random()*Integer.MAX_VALUE));
+        int temp = (int)(Math.random()*Integer.MAX_VALUE);
+        intsingleLinkedList.add(temp);
+        intsingleLinkedList.add((int)(Math.random()*Integer.MAX_VALUE));
+        intsingleLinkedList.removeNode(2);
+        assertEquals(temp, intsingleLinkedList.getNode(2));
+    }
+
+    @Test
+    void removeNodes() {
+        SingleLinkedList<Integer> intsingleLinkedList = new SingleLinkedList<>();
+        intsingleLinkedList.add((int)(Math.random()*Integer.MAX_VALUE));
+        intsingleLinkedList.add(5);
+        intsingleLinkedList.add((int)(Math.random()*Integer.MAX_VALUE));
+        intsingleLinkedList.add(5);
+        intsingleLinkedList.add((int)(Math.random()*Integer.MAX_VALUE));
+        intsingleLinkedList.add(5);
+        System.out.println(intsingleLinkedList);
+        intsingleLinkedList.removeNodes(5);
+        System.out.println(intsingleLinkedList);
+        assertEquals(3, intsingleLinkedList.getSize());
+
+    }
+
+    @Test
+    void getNode() {
+        SingleLinkedList<Integer> intsingleLinkedList = new SingleLinkedList<>();
+        intsingleLinkedList.add((int)(Math.random()*Integer.MAX_VALUE));
+        int value = (int)(Math.random()*Integer.MAX_VALUE);
+        intsingleLinkedList.add((int)(value));
+        intsingleLinkedList.add((int)(Math.random()*Integer.MAX_VALUE));
+        assertEquals(value, intsingleLinkedList.getNode(2));
+    }
+
+    @Test
+    void getSize() {
+        SingleLinkedList<Integer> intsingleLinkedList = new SingleLinkedList<>();
+        intsingleLinkedList.add((int)(Math.random()*Integer.MAX_VALUE));
+        intsingleLinkedList.add((int)(Math.random()*Integer.MAX_VALUE));
+        intsingleLinkedList.add((int)(Math.random()*Integer.MAX_VALUE));
+        assertEquals(3, intsingleLinkedList.getSize());
+    }
+
+    @Test
+    void iterator() {
+        SingleLinkedList<Integer> intsingleLinkedList = new SingleLinkedList<>();
+        intsingleLinkedList.add(5);
+        intsingleLinkedList.add(3);
+        intsingleLinkedList.add(8);
+        intsingleLinkedList.add(7);
+        Iterator<Integer> iterator = intsingleLinkedList.iterator();
+        assertTrue((iterator != null));
+        assertTrue(iterator.hasNext());
+        assertEquals(5, iterator.next());
+        assertEquals(3, iterator.next());
     }
 }
