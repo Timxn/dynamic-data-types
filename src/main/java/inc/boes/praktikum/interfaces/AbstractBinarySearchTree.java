f94ca0fc8f50e340d8fe756a5707ac9f2073acbe
Timon Stronczek <s6tnstro@uni-bonn.de>
2022-05-10 11:20:29 +0200

    update to comparator

--- src/main/java/inc/boes/praktikum/interfaces/AbstractBinarySearchTree.java
+++ src/main/java/inc/boes/praktikum/interfaces/AbstractBinarySearchTree.java
@@ -2,7 +2,7 @@ package inc.boes.praktikum.interfaces;
 
 import inc.boes.praktikum.classes.trees.TreeNode;
 
-public interface AbstractBinarySearchTree<T extends Number> {
+public interface AbstractBinarySearchTree<T> {
     void insert(T value);
     void delete(T value);
     boolean search(T value);
